730bfadb9c4b931393cc4df9bff8936a181d2d14
Jeff Bee <jeffreypbee@gmail.com>
2023-06-15 13:46:05 -0400

    Cleaning up classes

--- Bench Scheduler/src/main/data/WorkWeek.java
+++ Bench Scheduler/src/main/data/WorkWeek.java
@@ -4,27 +4,28 @@ import java.util.*;
 
 public class WorkWeek {
 
-    private Map<DayOfTheWeek, HashMap<Person, Boolean>> personWorkSchedule = new LinkedHashMap<DayOfTheWeek, HashMap<Person, Boolean>>();
+    private Map<DayOfTheWeek, ArrayList<ScheduledPerson>> personWorkSchedule = new LinkedHashMap<DayOfTheWeek, ArrayList<ScheduledPerson>>();
     private Map<DayOfTheWeek, LinkedList<Benchwork>> benchworkSchedule = new LinkedHashMap<DayOfTheWeek, LinkedList<Benchwork>>();
 
     public void scheduleWorkWeek() {
-        for (Map.Entry<DayOfTheWeek, LinkedList<Benchwork>> benchworkForTheDay : benchworkSchedule.entrySet()) {
-            for (Benchwork benchwork : benchworkForTheDay.getValue()) {
-                for (Map.Entry<Person, Boolean> personSchedule : personWorkSchedule.get(benchworkForTheDay.getKey()).entrySet()) {
-                    Person person = personSchedule.getKey();
-                    boolean isScheduled = personSchedule.getValue();
-                    if (!isScheduled && person.trainedOnBench(benchwork.getBench())) {
-                        benchwork.schedulePerson(person);
-                    }
-                }
-            }
-        }
-    }
 
+    }
+//    public void scheduleWorkWeek() {
+//        for (Map.Entry<DayOfTheWeek, LinkedList<Benchwork>> benchworkForTheDay : benchworkSchedule.entrySet()) {
+//            for (Benchwork benchwork : benchworkForTheDay.getValue()) {
+//                for (PersonWorking personWorking : personWorkSchedule.get(benchworkForTheDay.getKey())) {
+//                    if (!personWorking.isWorking() && personWorking.getPerson().trainedOnBench(benchwork.getBench())) {
+//                        benchwork.schedulePerson(personWorking.getPerson());
+//                    }
+//                }
+//            }
+//        }
+//    }
+//
     public void addToPersonWorkSchedule(Person person, LinkedList<DayOfTheWeek> daysWorking) {
-        for (Map.Entry<DayOfTheWeek, HashMap<Person, Boolean>> dayOfTheWeek : personWorkSchedule.entrySet()) {
+        for (Map.Entry<DayOfTheWeek, ArrayList<ScheduledPerson>> dayOfTheWeek : personWorkSchedule.entrySet()) {
             if (daysWorking.contains(dayOfTheWeek.getKey())) {
-                dayOfTheWeek.getValue().put(person, false);
+                dayOfTheWeek.getValue().add(new ScheduledPerson(person));
             }
         }
     }
