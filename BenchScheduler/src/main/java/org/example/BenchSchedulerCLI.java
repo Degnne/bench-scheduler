d2f78ed3c0d50a0a9814dce307789c3c7ea212cb
Jeff Bee <jeffreypbee@gmail.com>
2023-06-22 15:30:42 -0400

    Added DAO classes

--- BenchScheduler/src/main/java/org/example/BenchSchedulerCLI.java
+++ BenchScheduler/src/main/java/org/example/BenchSchedulerCLI.java
@@ -4,18 +4,18 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
 
-public class Main {
+public class BenchSchedulerCLI {
     public static void main(String[] args) {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:postgresql://localhost:5432/EmployeeProjects");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres1");
 
-        Main main = new Main(dataSource);
+        BenchSchedulerCLI main = new BenchSchedulerCLI(dataSource);
         main.run();
     }
 
-    public Main(DataSource dataSource) {
+    public BenchSchedulerCLI(DataSource dataSource) {
 
     }
 
