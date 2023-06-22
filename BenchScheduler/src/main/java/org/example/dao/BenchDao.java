86ebef1fab3b03c712b1301ddf41e73d7fdbb856
Jeff Bee <jeffreypbee@gmail.com>
2023-06-22 15:45:58 -0400

    Adding methods to DAO interfaces

--- BenchScheduler/src/main/java/org/example/dao/BenchDao.java
+++ BenchScheduler/src/main/java/org/example/dao/BenchDao.java
@@ -1,4 +1,16 @@
 package org.example.dao;
 
+import org.example.model.Bench;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface BenchDao {
+
+    public Bench getBenchById(int benchId);
+
+    public int getBenchRequirementsByBenchIdAndDate(int benchId, LocalDate date);
+
+    public List<Bench> getBenchesTrainedByPersonId(int personId);
+
 }
--- BenchScheduler/src/main/java/org/example/dao/PersonDao.java
+++ BenchScheduler/src/main/java/org/example/dao/PersonDao.java
@@ -1,4 +1,24 @@
 package org.example.dao;
 
+import org.example.model.Person;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface PersonDao {
+
+    public Person getPersonById(int personId);
+
+    public List<Person> getPeopleByTrainedBenchId(int benchId);
+
+    public List<Person> getPeopleWorkingByDate(LocalDate date);
+
+    public List<Person> getPeopleScheduledByDateAndBenchId(LocalDate date, int bench_id);
+
+    public int deletePersonById(int personId);
+
+    public Person updatePersonById(int personId);
+
+    public Person createPerson(Person person);
+
 }
