b01bcfcc2dad82786152eb720ec7f3ba375865bd
Jeff Bee <jeffreypbee@gmail.com>
2023-06-22 15:22:03 -0400

    Initial setup

--- BenchScheduler/src/main/java/org/example/Main.java
+++ BenchScheduler/src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.sql.DataSource;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/EmployeeProjects");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("postgres1");
+
+        Main main = new Main(dataSource);
+        main.run();
+    }
+
+    public Main(DataSource dataSource) {
+
+    }
+
+    public void run() {
+
     }
 }
---
+++ BenchScheduler/src/main/java/org/example/model/Bench.java
@@ -0,0 +1,23 @@
+package org.example.model;
+
+public class Bench {
+
+    private int bench_id;
+    private String bench_name;
+
+    public int getBench_id() {
+        return bench_id;
+    }
+
+    public void setBench_id(int bench_id) {
+        this.bench_id = bench_id;
+    }
+
+    public String getBench_name() {
+        return bench_name;
+    }
+
+    public void setBench_name(String bench_name) {
+        this.bench_name = bench_name;
+    }
+}
